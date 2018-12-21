f31bdf447cf3a0ad559ac04dce468437312ae2ff
l00pen <lovisa.skeppe@gmail.com>
2018-12-21 17:05:00 +0100

    Updates

---
+++ precache-manifest.d9189d3eae445fdb0f925d292c3ce2b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5197968207a6f0611f4b",
+    "url": "/gift-wrapper/static/js/runtime~main.51979682.js"
+  },
+  {
+    "revision": "da5b7071173beca54dd6",
+    "url": "/gift-wrapper/static/js/main.da5b7071.chunk.js"
+  },
+  {
+    "revision": "a710dc7c6f3a7c649234",
+    "url": "/gift-wrapper/static/js/1.a710dc7c.chunk.js"
+  },
+  {
+    "revision": "da5b7071173beca54dd6",
+    "url": "/gift-wrapper/static/css/main.4a70cad3.chunk.css"
+  },
+  {
+    "revision": "ebdd43b8495ce0de6aa2f05eead1cef0",
+    "url": "/gift-wrapper/index.html"
+  }
+];
